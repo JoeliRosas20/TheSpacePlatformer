5966a54645d48c9fe3adf81a6c4af84e2e1b0c5a
Joel Rosas <joeli.rosas20@gmail.com>
2021-08-31 19:03:57 -0400

    Adding the true Game Frame

--- src/Engine/Sprite.java
+++ src/Engine/Sprite.java
@@ -1,4 +1,22 @@
 package Engine;
 
 public class Sprite {
+
+    private Animation animation;
+    private float x;
+    private float y;
+    private float dx;
+    private float dy;
+
+    public Sprite(Animation animation){
+        this.animation = animation;
+    }
+
+    public void update(long elapsedTime){
+        x += dx * elapsedTime;
+        y += dy * elapsedTime;
+    }
+
+
+
 }
